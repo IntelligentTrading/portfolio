ee9828cdb3f02414d03cd9b4ef974980ff35ec9a
Tom Counsell <other@tomcounsell.com>
2018-11-08 11:46:09 +0100

    load static files

--- settings/__init__.py
+++ settings/__init__.py
@@ -163,6 +163,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR+"static/",
+]
+
 
 try:
     from settings.vendor_settings import *
