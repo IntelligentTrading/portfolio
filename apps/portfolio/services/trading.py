1ef360237ce3ff31c253a8c1b489291fd5d22bc2
Tom Counsell <other@tomcounsell.com>
2018-11-08 19:58:39 +0100

    add binance set function

--- apps/portfolio/services/trading.py
+++ apps/portfolio/services/trading.py
@@ -9,7 +9,6 @@ def get_binance_portfolio_data(binance_account):
     api_url_base = ITF_TRADING_API_URL
     api_url = api_url_base + "portfolio/"
     headers = {'Content-Type': 'application/json'}
-
     data = {
         "api_key": ITF_TRADING_API_KEY,
         "binance": {
@@ -29,3 +28,27 @@ def get_binance_portfolio_data(binance_account):
         data = response.json()
         return data['binance']
 
+
+def set_binance_portfolio(binance_account, allocations):
+    api_url_base = ITF_TRADING_API_URL
+    api_url = api_url_base + "portfolio/"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "api_key": ITF_TRADING_API_KEY,
+        "binance": {
+            "api_key": binance_account.api_key,
+            "secret_key": binance_account.secret_key,
+            "type": "market",
+            "allocations": json.dumps(allocations),
+        }
+    }
+
+    if DEBUG:
+        logging.debug(data)
+
+    response = requests.put(api_url, json=data, headers=headers)
+    logging.debug(response.text)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
