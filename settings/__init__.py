fcf0291838a7de36eb775f813cd0ce0f7f6ab409
Tom Counsell <other@tomcounsell.com>
2018-11-10 09:06:46 +0100

    middleware ordering

--- settings/__init__.py
+++ settings/__init__.py
@@ -90,13 +90,13 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # for static files
 ]
 
 ROOT_URLCONF = 'settings.urls'
