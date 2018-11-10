3dbf9f3772111e1262b2556c744ffa57773399a2
Tom Counsell <other@tomcounsell.com>
2018-11-10 09:10:18 +0100

    remove compression

--- settings/__init__.py
+++ settings/__init__.py
@@ -165,7 +165,7 @@ STATIC_ROOT = os.path.join(SITE_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from settings.vendor_settings import *
